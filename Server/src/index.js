85e520e03314869fe7c35948d8dcd7f292eeea8c
Jose-Chinola <reny25057@gmail.com>
2025-05-12 14:55:50 -0400

    update

--- Server/src/index.js
+++ Server/src/index.js
@@ -6,15 +6,3 @@ getConnection().then(() => {
         console.log(`Server running on port ${process.env.PORT}`);
     })
 });
-
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Emula __dirname en ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Ahora apunta directo a tu carpeta controllers
-const controllersDir = path.join(__dirname, "controllers");
-console.log("⮕ controllers folder contains:", fs.readdirSync(controllersDir));
